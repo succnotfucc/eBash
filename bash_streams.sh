8faf431cd91d66cbaa5f0f700946a10206bc7e47
succnotfucc <119111813+succnotfucc@users.noreply.github.com>
2023-04-27 21:16:51 +0300

    Delete bash_streams.sh

--- bash_streams.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#if no arguments were passed, ls will be executed for the current directory
-
-ls $@ 1>logs/ls-success.log 2>logs/ls-error.log # "@" is used as an array os passed arguments
